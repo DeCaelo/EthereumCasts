ad7684107d78002057f08a007e98ede4fdc59e30
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-01-16 10:31:13 +0100

    Testing Warmup & provider add to works

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
@@ -11,22 +13,32 @@ let factory;
 let campaignAddress;
 let campaign;
 
-beforeEach(async () = > {
-    accounts = await web3.eth.getAccounts();
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  // Use one of those accounts to deploy the contract
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
 
-    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-        .deploy({ data: compiledFactory.bytecode })
-        .send({ from: accounts[0], gas: '1000000'});
+  factory.setProvider(provider);
 
-    await factory.methods.createCampaign('100').send({
-        from: account[0],
-        gas: '1000000'
-    });
-    // return an array of addresses
-    [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+  await factory.methods.createCampaign('100').send({
+    from: accounts[0],
+    gas: '1000000',
+  });
+  // return an array of addresses
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
 
-    campaign = await new web3.eth.Contract(
-        JSON.parse(compiledCampaign.interface),
-        campaignAddress
-    );
-})
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress,
+  );
+});
+
+describe('Campaigns', () => {
+  it('deploys a factory and a campaign', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+});
