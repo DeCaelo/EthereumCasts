5edbf597d63430b92efc8d87fe99d3faf5ac05fe
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-01-16 14:46:21 +0100

    Server vs Client Web3 Instances

--- kickstart/ethereum/web3.js
+++ kickstart/ethereum/web3.js
@@ -1,5 +1,16 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+// check if it's browser and if metamask have injected web3
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  // We are in the browser and metamask is running.
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // We are in the server or the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/vetUIoaE0kpbIU2N9vvn',
+  );
+  web3 = new Web3(provider);
+}
 
 export default web3;
