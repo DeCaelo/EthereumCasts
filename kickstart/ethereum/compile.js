4719c28510b641a64f68b8ae18dd4ac44c1d3cfb
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-01-16 09:48:34 +0100

    Single Run Compilation

---
+++ kickstart/ethereum/compile.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+// build path
+const buildPath = path.resolve(__dirname, 'build');
+// delete if exist
+fs.removeSync(buildPath);
+// campaign.sol path
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+// Read campaign.sol
+const source = fs.readFileSync(campaignPath, 'utf8');
+// Compile both contracts with solidity compiler
+const output = solc.compile(source, 1).contracts;
+// Write output to the build directory
+fs.ensureDirSync(buildPath);
+// check all contract in campaign.sol
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract + '.json'),
+    output[contract],
+  );
+}
