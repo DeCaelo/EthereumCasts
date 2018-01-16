4536b3e7f822c8e81f55f2c08b443f373143cb7a
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-01-16 16:53:16 +0100

    Final CSS Fix

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 import Layout from '../components/Layout';
-// import 'semantic-ui-css/semantic.min.css';
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -27,10 +26,6 @@ class CampaignIndex extends Component {
     return (
       <Layout>
         <div>
-          <link
-            rel="stylesheet"
-            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
-          />
           <h3>Open Campaigns</h3>
           <Button
             floated="right"
