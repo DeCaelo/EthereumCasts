affde91acae74b46be6078e916cc9c4dd32ff59d
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-01-16 14:24:23 +0100

    Fetching Deployed Campaigns

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import factory from '../ethereum/factory';
 
-export default () => {
-  return <h1>This is the new campaign page!!!</h1>;
-};
+class CampaignIndex extends Component {
+  async componentDidMount() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+
+    console.log(campaigns);
+  }
+
+  render() {
+    return <h1>CampaignIndex</h1>;
+  }
+}
+
+export default CampaignIndex;
