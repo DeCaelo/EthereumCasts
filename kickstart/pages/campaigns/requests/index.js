8ab67bd3a8a5150fbee4611996623767711fba5d
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2018-01-18 11:44:21 +0100

    Rendering a Table

--- kickstart/pages/campaigns/requests/index.js
+++ kickstart/pages/campaigns/requests/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
 import Campaign from '../../../ethereum/campaign';
@@ -18,12 +18,12 @@ class RequestIndex extends Component {
         }),
     );
 
-    console.log(requests);
-
     return { address, requests, requestCount };
   }
 
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
+
     return (
       <Layout>
         <h3>Request List</h3>
@@ -32,6 +32,19 @@ class RequestIndex extends Component {
             <Button secondary>Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+        </Table>
       </Layout>
     );
   }
